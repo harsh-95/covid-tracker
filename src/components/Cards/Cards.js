11cf02f42a41d7928a84b83ef1d71955eeacaf5e
Harsh Gupta <harshgupta951114@gmail.com>
2020-04-14 19:07:21 +0530

    use material grid and card

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 
-const Cards = (props) => {
-    console.log(props);
+import styles from './Cards.module.css';
+
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+
+    if(!confirmed){
+        return null;
+    }
     return(
-        <div><h1>Cards</h1></div>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">Infected</Typography>
+                        <Typography variant="h6">{confirmed.value}</Typography>
+                        <Typography color="textSecondary">Date</Typography>
+                        <Typography variant="body2">No. of Infected cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">Recovered</Typography>
+                        <Typography variant="h6">{recovered.value}</Typography>
+                        <Typography color="textSecondary">Date</Typography>
+                        <Typography variant="body2">No. of Recovered cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary">Deaths</Typography>
+                        <Typography variant="h6">{deaths.value}</Typography>
+                        <Typography color="textSecondary">Date</Typography>
+                        <Typography variant="body2">No. of deaths</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
