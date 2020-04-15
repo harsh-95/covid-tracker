7cf309680b38264d485d40cbf82eef65902af303
Harsh Gupta <harshgupta951114@gmail.com>
2020-04-15 20:18:32 +0530

    Updates

---
+++ precache-manifest.2953d6f3622b2441b95acd75f7f65d4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad9e84ea83a018adc9893e3a03db816",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "091c1d8a754e8081c3c2",
+    "url": "/covid-tracker/static/css/main.6b01bcc9.chunk.css"
+  },
+  {
+    "revision": "4c55fad1191ff4534c21",
+    "url": "/covid-tracker/static/js/2.2ce74ae6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.2ce74ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091c1d8a754e8081c3c2",
+    "url": "/covid-tracker/static/js/main.5fc43479.chunk.js"
+  },
+  {
+    "revision": "5767bd1d46dd2aefcfdb",
+    "url": "/covid-tracker/static/js/runtime-main.63c14202.js"
+  },
+  {
+    "revision": "017560a09f00ba7db245b8d15b9f19f8",
+    "url": "/covid-tracker/static/media/covid-19-logo.017560a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/harsh-95/covid-tracker/precache-manifest.ad235aa53d46a9b66c493875b671bd51.js"
+  "/covid-tracker/precache-manifest.2953d6f3622b2441b95acd75f7f65d4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/harsh-95/covid-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
