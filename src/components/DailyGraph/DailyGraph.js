8d32505531df8f51c2b0646dfaf58910320e23d7
Harsh Gupta <harshgupta951114@gmail.com>
2020-04-22 00:25:58 +0530

    latest cases updations added to statewise table

--- src/components/DailyGraph/DailyGraph.js
+++ src/components/DailyGraph/DailyGraph.js
@@ -96,14 +96,33 @@ const DailyGraph = () =>{
                         </tr>
                     </thead>
                     <tbody>
-                        {stateWise.map(({state, confirmed, active, recovered, deaths},i) => 
+                        {stateWise.map(({state, confirmed, active, recovered, deaths, deltaconfirmed, deltarecovered, deltadeaths},i) => 
                                     state !== "Total" ?
                                     (<tr key={i}>
-                                        <td className={styles.left}>{state}</td>
-                                        <td className={styles.right}>{confirmed}</td>
-                                        <td className={styles.right}>{active}</td>
-                                        <td className={styles.right}>{recovered}</td>
-                                        <td className={styles.right}>{deaths}</td>
+                                        <td className={styles.left}>
+                                            {state}
+                                        </td>
+                                        <td className={styles.right}>
+                                            <span className={cx(styles.delta, styles.confirmed)}>
+                                                {deltaconfirmed !== "0" ? "⯅"+deltaconfirmed: null}
+                                            </span>
+                                            <span>{confirmed}</span>
+                                        </td>
+                                        <td className={styles.right}>
+                                            <span>{active}</span>
+                                        </td>
+                                        <td className={styles.right}>
+                                            <span className={cx(styles.delta, styles.recovered)}>
+                                                {deltarecovered !== "0" ? "⯅"+deltarecovered: null}
+                                            </span>
+                                            <span>{recovered}</span>
+                                        </td>
+                                        <td className={styles.right}>
+                                            <span className={cx(styles.delta, styles.deaths)}>
+                                                {deltadeaths !== "0" ? "⯅"+deltadeaths: null}
+                                            </span>   
+                                            <span>{deaths}</span>
+                                        </td>
                                     </tr>)
                                     : null
                                     )}
@@ -114,13 +133,29 @@ const DailyGraph = () =>{
 
     const totalCasesHeader = (
         <div>
-            {stateWise.map(({state, confirmed, active, recovered, deaths},i) => 
+            {stateWise.map(({state, confirmed, active, recovered, deaths, deltaconfirmed, deltarecovered, deltadeaths},i) => 
                                     state === "Total" ?
-                                    (<div className={styles.header}>
-                                        <div className={cx(styles.label, styles.confirmed)}><p>Confirmed</p><p>{confirmed}</p></div>
-                                        <div className={cx(styles.label, styles.active)}><p>Active</p><p>{active}</p></div>
-                                        <div className={cx(styles.label, styles.recovered)}><p>Recovered</p><p>{recovered}</p></div>
-                                        <div className={cx(styles.label, styles.deaths)}><p>Deaths</p><p>{deaths}</p></div>
+                                    (<div className={styles.header} key={i}>
+                                        <div className={cx(styles.label, styles.confirmed)}>
+                                            <div>Confirmed</div>
+                                            <div>⯅{deltaconfirmed}</div>
+                                            <div>{confirmed}</div>
+                                        </div>
+                                        <div className={cx(styles.label, styles.active)}>
+                                            <div>Active</div>
+                                            <div></div>
+                                            <div>{active}</div>
+                                        </div>
+                                        <div className={cx(styles.label, styles.recovered)}>
+                                            <div>Recovered</div>
+                                            <div>⯅{deltarecovered}</div>
+                                            <div>{recovered}</div>
+                                        </div>
+                                        <div className={cx(styles.label, styles.deaths)}>
+                                            <div>Deaths</div>
+                                            <div>⯅{deltadeaths}</div>
+                                            <div>{deaths}</div>
+                                        </div>
                                      </div>)
                                     : null
                                     )}
